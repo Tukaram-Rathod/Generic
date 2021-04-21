3d6efc3b17282b43a1e17e04307c6c2730402d99
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-21 23:27:43 +0530

    Added. TC1.2 Given Max Number at 2st Position return the Same Number

--- src/test/java/com/comparison/CompareTest.java
+++ src/test/java/com/comparison/CompareTest.java
@@ -7,16 +7,25 @@ import org.junit.Test;
 public class CompareTest {
 
     @Test
-    public void givenInteger_ShouldReturnsLargest(){
+    public void givenInteger_ShouldReturnsLargest() {
         Comparison comparison = new Comparison();
-        int max = comparison.largest(41,56,23);
-        Assert.assertEquals(56,max);
+        int max = comparison.largest(41, 56, 23);
+        Assert.assertEquals(56, max);
     }
+
     @Test
     //TC1.1 Ensure the test data has Max Number in the First position
     public void givenInteger_AndFirstNumberIsMax_ShouldReturn_true() {
         Comparison comparison = new Comparison();
-        int max =  comparison.largest(555, 104, 180);
+        int max = comparison.largest(555, 104, 180);
         Assert.assertEquals(555, max);
+    }
+
+    @Test
+    //TC1.2 Ensure the test data has Max Number in the Second position
+    public void givenInteger_AndSecondNumberIsMax_ShouldReturn_true() {
+        Comparison comparison = new Comparison();
+        int max = comparison.largest(55, 704, 180);
+        Assert.assertEquals(704, max);
     }
 }
