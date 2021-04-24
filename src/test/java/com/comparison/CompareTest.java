5df7ed9d9d3bb1b5de97d2980971ab519969842c
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-24 19:10:23 +0530

    Added. Extend the max

--- src/test/java/com/comparison/CompareTest.java
+++ src/test/java/com/comparison/CompareTest.java
@@ -4,34 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CompareTest {
-    // UC1-To Validate given three integer value gives maximum return true
-    @Test
-    public void givenInteger_ShouldReturnsLargest() {
-        Comparison comparison = new Comparison();
-        int max = comparison.largest(41, 56, 23);
-        Assert.assertEquals(56, max);
-    }
-    // UC2-To Validate given three float value gives maximum return true
-    @Test
-    public void givenFloat_ShouldReturnsLargest() {
-        Comparison comparison = new Comparison();
-        Float max = comparison.largest(2.0f, 4.51f, 6.8f);
-        //Assert.assertEquals(6.8f,max);
 
-    }
-    // UC3-To Validate given three String value gives maximum return true
+    Comparison comparison = new Comparison();
+    //UC4 :Extend the max method to take more then three parameters
     @Test
-    public void givenString_ShouldReturnsLargest() {
-        Comparison comparison = new Comparison();
-        String max = comparison.largest("Ashish", "Rathod","Aarti");
-        Assert.assertEquals("Rathod",max);
-    }
-
-    // Refactor 1:Refactor all the 3 to One Generic Method and find the maximum
-    @Test
-    public void givenObjectGenericMethod_ShouldReturnsLargest() {
-        Comparison comparison = new Comparison();
-        String max = comparison.largest("Apple", "Peach","Banana");
-        Assert.assertEquals("Peach",max);
+    public void givenThreeNumber_AndFirstNumber_ShouldReturnsLargest_True() {
+        Integer [] integers = {14,5,3,6,8};
+        Integer max = comparison.largest(integers);
+        Assert.assertEquals(Integer.valueOf(14),max);
     }
 }
